cdca0b55b1eb8d767826573dd2a7326bac2b20da
Alex Kiptoo Kitur <160272847+alexkkitur@users.noreply.github.com>
2026-06-25 17:49:16 +0300

    Update database.php

--- laravel-backend/config/database.php
+++ laravel-backend/config/database.php
@@ -1,3 +1,8 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'mysql-20c016ac-al-2d1d.h.aivencloud.com'),
@@ -17,3 +22,21 @@
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ],
         ],
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => true,
+        ],
+    ],
+    'migrations' => 'migrations',
+    'redis' => [
+        'client' => 'phpredis',
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+    ],
+];
